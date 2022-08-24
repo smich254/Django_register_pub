0f3926b1a4eb043b658690a6875efc52070d387c
smich254 <smich254@gmail.com>
2022-08-23 22:30:02 -0500

    agregando funcionalidad a app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app=Flask(__name__)
+
+@app.before_request
+def before_request():
+    print("Antes de la peticion ...")
+
+@app.after_request
+def after_request():
+    print("Despues de la peticion")
 
 @app.route('/')
 def index():
@@ -23,5 +31,18 @@ def contacto(nombre, edad):
     }
     return render_template('contacto.html', data=data)
 
+def query_string():
+    print(request)
+    print(request.args)
+    print(request.args.get('param1'))
+    print(request.args.get('param2'))
+    return "Ok"
+
+def pagina_no_encontrada(error):
+    #return render_template('404.html'), 404
+    return redirect(url_for('index'))    
+
 if __name__ == '__main__':
+    app.add_url_rule('/query_string', view_func=query_string)
+    app.register_error_handler(404, pagina_no_encontrada)
     app.run(debug=True, port=5000)
